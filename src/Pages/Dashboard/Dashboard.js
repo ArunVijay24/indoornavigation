1e062cfccebafdb33f15905761918d1fd646a1ff
Arunachalam Vijayakumar <arunachalamvijayakumar@cloudix.io>
2022-09-22 18:52:10 +0530

    Added conditon for validhighlights

--- src/Pages/Dashboard/Dashboard.js
+++ src/Pages/Dashboard/Dashboard.js
@@ -5,12 +5,32 @@ import { Media } from 'reactstrap';
 import { Card, Select, Space } from 'antd';
 import Axios from 'axios';
 import _isEmpty from 'lodash/isEmpty';
+import moment from 'moment/moment';
 const { Option } = Select;
 
 const Dashboard = () => {
-	const [ malls, setMalls ] = useState([]),
+	const [ malls, setMalls ] = useState([
+			{
+				ID: 1,
+				MALL_NAMES: 'Phoenix-Mall',
+				LINKS:
+					'https://dynamic-media-cdn.tripadvisor.com/media/photo-o/08/2a/98/15/phoenix-marketcity-mumbai.jpg?w=1200&h=-1&s=1'
+			},
+			{
+				ID: 2,
+				MALL_NAMES: 'Nexus-Mall',
+				LINKS:
+					'https://www.nexusmalls.com/resources/assets/coporate/images/forum-kormangala/forum-kormangala-img2.jpg'
+			},
+			{
+				ID: 3,
+				MALL_NAMES: 'Marina-Mall',
+				LINKS: 'https://www.olympiagroup.in/olympia-panache/img/map/Final/THE-MARINA-MALL.jpg'
+			}
+		]),
 		[ mallData, setMallData ] = useState([]),
-		[ image, setImage ] = useState(imageurl);
+		[ image, setImage ] = useState(imageurl),
+		[ mallName, setMallName ] = useState('');
 
 	const mallsurl = 'http://192.168.68.123:3000/FindMyWay/api/test/malls';
 	useEffect(() => {
@@ -30,6 +50,7 @@ const Dashboard = () => {
 	const onChange = (value) => {
 		let img = malls.filter((mall) => mall.ID === value);
 		setImage(img[0].LINKS);
+		setMallName(img[0].MALL_NAMES);
 		Axios({
 			method: 'get',
 			url: getbymallsurl,
@@ -43,10 +64,18 @@ const Dashboard = () => {
 			});
 	};
 
+	const validHighlights = mallData.filter(
+		(data) =>
+			moment(data.START_DATE).isBefore(moment()) ||
+			(moment(data.START_DATE).isSame(moment()) && moment(data.END_DATE).isAfter(moment())) ||
+			moment(data.END_DATE).isSame(moment())
+	);
+
+	console.log('valhigh', validHighlights);
+
 	return (
 		<div className="content">
-			<div className="site-layout-content">
-				<h1>Welcome to Dashboard</h1>
+			<div className="mall">
 				<Select
 					showSearch
 					placeholder="Select a Mall"
@@ -60,22 +89,24 @@ const Dashboard = () => {
 						</Option>
 					))}
 				</Select>
+				<Media className="text-center">
+					<img
+						src={image}
+						style={{
+							width: '100px',
+							height: '100px',
+							borderRadius: '50%',
+							border: `3px solid #f4f5fa`
+						}}
+						alt="dashboard"
+						className="shadow  p-3"
+					/>
+				</Media>
 				<Space>
-					<Media className="text-center">
-						<img
-							src={image}
-							style={{
-								width: '100px',
-								height: '100px',
-								borderRadius: '50%',
-								border: `3px solid #f4f5fa`
-							}}
-							alt="dashboard"
-							className="shadow  p-3"
-						/>
-					</Media>
+					<h1>{`Welcome to ${mallName ? mallName : 'Dashboard'} `}</h1>
 				</Space>
 			</div>
+			<div className="site-layout-content" />
 			<Space>
 				{mallData.map((mall) => (
 					<Card key={mall.ID} extra={<a href="#">More</a>} style={{ width: 300, height: 200 }}>
